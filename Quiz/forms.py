5a71aab81f0e172cf3d78ebd7c2733ab22796881
Uvini Ranaweera <ranaweeraraua.19@uom.lk>
2022-10-06 22:30:57 +0530

    Partially completed question form

--- Quiz/forms.py
+++ Quiz/forms.py
@@ -14,6 +14,11 @@ class CreateQuizForm(forms.ModelForm):
     # quiz_name = forms.CharField(max_length=50)
     # instructions = forms.CharField(max_length=200, widget=forms.Textarea(attrs={'rows': 4, 'cols': 30}))
 
-class AddQuestion(forms.Form):
+class AddQuestion(forms.ModelForm):
+    quizID=forms.ModelChoiceField(queryset=models.Quiz.objects.all(),empty_label="quiz_name", to_field_name="id")
     class Meta:
         model = models.Question
+        fields = ['quiz', 'question', 'option1', 'option2', 'option3', 'answer']
+        widgets = {
+            'question': forms.Textarea(attrs={'rows': 3, 'cols': 50})
+        }
--- Quiz/views.py
+++ Quiz/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from . import forms as tforms
+from . import models as tmodel
 
 # Create your views here.
 def create(request):
@@ -15,7 +16,20 @@ def create(request):
     return create_quiz_view(request)
 
 def qa(request):
-    return render(request, 'Teacher_QA/QA.html')
+    def add_question_view(request):
+        questionForm = tforms.AddQuestion()
+        if request.method=='POST':
+            questionForm=tforms.AddQuestion(request.POST)
+            if questionForm.is_valid():
+                question=questionForm.save(commit=False)
+                quiz=tmodel.Quiz.objects.get(id=request.POST.get('quizID'))
+                question.quiz=quiz
+                question.save()       
+            else:
+                print("form is invalid")
+            # return HttpResponseRedirect('/teacher/teacher-view-question')
+        return render(request,'Teacher_QA/QA.html',{'questionForm':questionForm})
+    return add_question_view(request)
 
 def summary(request):
     return render(request, 'Teacher_Quiz_Summary/Quiz-Summary.html')
@@ -29,4 +43,3 @@ def dashboard(request):
 def create_quiz_view(request):
     createQuizForm = tforms.CreateQuizForm()
     return render(request,'Create_Quiz/Create_Quiz.html',{'createQuizForm':createQuizForm})
-
