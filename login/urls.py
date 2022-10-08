013d41a0de1af439b0cea4f589ee61b22dc542be
Uvini Ranaweera <ranaweeraraua.19@uom.lk>
2022-10-08 17:55:37 +0530

    signup page completed

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('tlogin', views.tlogin, name='tlogin'),
-    path('slogin',views.slogin, name='slogin')
+    # path('tlogin', views.tlogin, name='tlogin'),
+    path('tlogin', LoginView.as_view(template_name='tlogin.html'),name='tlogin'),
+    # path('slogin',views.slogin, name='slogin')
 ]
--- login/views.py
+++ login/views.py
@@ -1,50 +1,51 @@
-from django.shortcuts import render
-import mysql.connector as sql
+# from django.shortcuts import render
+# import mysql.connector as sql
 
-em = ''
-pwd = ''
+# em = ''
+# pwd = ''
 
-# Create your views here.
-def tlogin(request):
-    global em, pwd
-    if request.method == "POST":
-        m = sql.connect(host="localhost", user="root", passwd="", database='quizdb')
-        cursor = m.cursor()
-        d = request.POST
-        for key, value in d.items():
-            if key == "email":
-                em = value
-            if key == "password":
-                pwd = value
+# # Create your views here.
+# def tlogin(request):
+#     global em, pwd
+#     if request.method == "POST":
+#         m = sql.connect(host="localhost", user="root", passwd="", database='quizdb')
+#         cursor = m.cursor()
+#         d = request.POST
+#         for key, value in d.items():
+#             if key == "email":
+#                 em = value
+#             if key == "password":
+#                 pwd = value
 
-        c = "select * from Quiz_teacher where email='{}' and password='{}'".format(em, pwd)
-        cursor.execute(c)
-        t = tuple(cursor.fetchall())
-        if t == ():
-            return render(request, 'tlogin.html')
-        else:
-            return render(request, "Teacher-Dashboard/Teacher_Dashboard.html")
+#         c = "select * from Quiz_teacher where email='{}' and password='{}'".format(em, pwd)
+#         cursor.execute(c)
+#         t = tuple(cursor.fetchall())
+#         if t == ():
+#             return render(request, 'tlogin.html')
+#         else:
+#             return render(request, "Teacher-Dashboard/Teacher_Dashboard.html")
 
-    return render(request, 'tlogin.html')
+#     return render(request, 'tlogin.html')
 
-def slogin(request):
-    global em, pwd
-    if request.method == "POST":
-        m = sql.connect(host="localhost", user="root", passwd="", database='quizdb')
-        cursor = m.cursor()
-        d = request.POST
-        for key, value in d.items():
-            if key == "email":
-                em = value
-            if key == "password":
-                pwd = value
+# def slogin(request):
+#     global em, pwd
+#     if request.method == "POST":
+#         m = sql.connect(host="localhost", user="root", passwd="", database='quizdb')
+#         cursor = m.cursor()
+#         d = request.POST
+#         for key, value in d.items():
+#             if key == "email":
+#                 em = value
+#             if key == "password":
+#                 pwd = value
 
-        c = "select * from student where email='{}' and password='{}'".format(em, pwd)
-        cursor.execute(c)
-        t = tuple(cursor.fetchall())
-        if t == ():
-            return render(request, 'slogin.html')
-        else:
-            return render(request, "Teacher-Dashboard/Teacher_Dashboard.html")
+#         c = "select * from student where email='{}' and password='{}'".format(em, pwd)
+#         cursor.execute(c)
+#         t = tuple(cursor.fetchall())
+#         if t == ():
+#             return render(request, 'slogin.html')
+#         else:
+#             return render(request, "Teacher-Dashboard/Teacher_Dashboard.html")
 
-    return render(request, 'slogin.html')
+#     return render(request, 'slogin.html')
+
